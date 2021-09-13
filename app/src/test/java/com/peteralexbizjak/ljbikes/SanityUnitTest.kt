a7b769849efbb71da4b1ae2f17b975f9f5b1050e
Peter Aleksander Bizjak <peter.aleksander.bizjak@ipmgroup.si>
2021-09-13 10:27:52 +0200

    Initial commit

---
+++ app/src/test/java/com/peteralexbizjak/ljbikes/SanityUnitTest.kt
@@ -0,0 +1,11 @@
+package com.peteralexbizjak.ljbikes
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SanityUnitTest {
+    @Test
+    fun universeWorks() {
+        assertEquals(true, true)
+    }
+}
