492c9734d7e691dba4543fd2505876c624a1ed14
Peter Aleksander Bizjak <peter.aleksander.bizjak@ipmgroup.si>
2021-09-16 22:51:24 +0200

    Implement logic for refreshing access tokens every one hour (probably good enough)

--- app/src/main/java/com/peteralexbizjak/ljbikes/api/repositories/implementations/TokenRepository.kt
+++ app/src/main/java/com/peteralexbizjak/ljbikes/api/repositories/implementations/TokenRepository.kt
@@ -6,13 +6,27 @@ import com.peteralexbizjak.ljbikes.api.models.tokens.RefreshTokenRequestModel
 import com.peteralexbizjak.ljbikes.api.models.tokens.TokenRequestModel
 import com.peteralexbizjak.ljbikes.api.repositories.ITokenRepository
 import com.peteralexbizjak.ljbikes.api.services.TokenService
+import java.time.Duration
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 internal class TokenRepository(
     private val tokenService: TokenService,
     private val sharedPreferences: SharedPreferences
 ) : ITokenRepository {
     override suspend fun persistClientTokens() {
-        if (sharedPreferences.contains(REFRESH_TOKEN) && sharedPreferences.contains(ACCESS_TOKEN)) {
+        if (sharedPreferences.contains(LAST_UPDATE) && sharedPreferences.contains(REFRESH_TOKEN)) {
+            val lastDateTime = LocalDateTime.parse(sharedPreferences.getString(LAST_UPDATE, null))
+            val currentDateTime = LocalDateTime.now()
+            val difference = Duration.between(lastDateTime, currentDateTime).toHours()
+            if (difference > 1L) {
+                with(sharedPreferences.edit()) {
+                    putString(LAST_UPDATE, currentDateTime.format(DateTimeFormatter.ISO_DATE_TIME))
+                    apply()
+                }
+                refreshAccessToken()
+            }
+
             return
         }
 
@@ -21,6 +35,7 @@ internal class TokenRepository(
         with(sharedPreferences.edit()) {
             putString(REFRESH_TOKEN, resultModel.refreshToken)
             putString(ACCESS_TOKEN, resultModel.token)
+            putString(LAST_UPDATE, LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME))
             apply()
         }
     }
@@ -40,5 +55,6 @@ internal class TokenRepository(
     companion object {
         private const val REFRESH_TOKEN = "refresh_token"
         private const val ACCESS_TOKEN = "access_token"
+        private const val LAST_UPDATE = "last_update"
     }
 }
