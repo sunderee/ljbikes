3b9fe208f764daaa3a900f83abd281ea73266d79
Peter Aleksander Bizjak <peter.aleksander.bizjak@ipmgroup.si>
2021-09-19 09:14:16 +0200

    Remove unnecessary code

--- app/src/main/java/com/peteralexbizjak/ljbikes/utils/JsonExt.kt
+++ app/src/main/java/com/peteralexbizjak/ljbikes/utils/JsonExt.kt
@@ -1,17 +1,8 @@
 package com.peteralexbizjak.ljbikes.utils
 
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 internal val json = Json {
     ignoreUnknownKeys = true
     isLenient = true
 }
-
-@ExperimentalSerializationApi
-internal inline fun <reified T> T.serialize(): String = json.encodeToString(this)
-
-@ExperimentalSerializationApi
-internal inline fun <reified T> String.deserialize(): T = json.decodeFromString(this)
